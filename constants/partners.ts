f7e16871c55f02d7c6eceec0af6e0d24c7478da9
Gabeen Yoon <teukday0701@naver.com>
2026-08-02 16:41:32 +0900

    chore: partners added

--- constants/partners.ts
+++ constants/partners.ts
@@ -1,42 +1,45 @@
-import bass from 'public/images/partners/bass.png';
-import altos from 'public/images/partners/altos.jpg';
-import dcamp from 'public/images/partners/dcamp.png';
-import crimson from 'public/images/partners/crimson.png';
-import fast from 'public/images/partners/fast.png';
-import eo from 'public/images/partners/eo.png';
-import futureplay from 'public/images/partners/futureplay.png';
-import startupStation from 'public/images/partners/startupStation.png';
-import campustown from 'public/images/partners/campusTown.png';
-import codeit from 'public/images/partners/codeit.png';
-import mesher from 'public/images/partners/mesher.png';
-import mashup from 'public/images/partners/mashup_ventures.svg';
-import aihub from 'public/images/partners/aihub.jpg';
-import zd from 'public/images/partners/zd.png';
-import koreaindustry from 'public/images/partners/koreaindustry.jpeg';
-import dunamu from 'public/images/partners/dunamu2.png';
-import fastventures from 'public/images/partners/fastventures2.png';
-import abovetech from 'public/images/partners/abovetech.png';
-import nonce from 'public/images/partners/nonce.png';
-import desker from 'public/images/partners/desker.svg';
+import bass from "public/images/partners/bass.png";
+import altos from "public/images/partners/altos.jpg";
+import dcamp from "public/images/partners/dcamp.png";
+import crimson from "public/images/partners/crimson.png";
+import fast from "public/images/partners/fast.png";
+import eo from "public/images/partners/eo.png";
+import futureplay from "public/images/partners/futureplay.png";
+import startupStation from "public/images/partners/startupStation.png";
+import campustown from "public/images/partners/campusTown.png";
+import codeit from "public/images/partners/codeit.png";
+import mesher from "public/images/partners/mesher.png";
+import mashup from "public/images/partners/mashup_ventures.svg";
+import aihub from "public/images/partners/aihub.jpg";
+import zd from "public/images/partners/zd.png";
+import koreaindustry from "public/images/partners/koreaindustry.jpeg";
+import dunamu from "public/images/partners/dunamu2.png";
+import fastventures from "public/images/partners/fastventures2.png";
+import abovetech from "public/images/partners/abovetech.png";
+import nonce from "public/images/partners/nonce.png";
+import desker from "public/images/partners/desker.svg";
+import strongventures from "public/images/partners/strongventures.png";
+import zuzu from "public/images/partners/zuzu.png";
 
 export const Partners = [
-    { name: 'bass', src: bass },
-    // { name: 'codeit', src: codeit },
-    { name: 'futureplay', src: futureplay },
-    { name: 'mashup', src: mashup },
-    { name: 'dcamp', src: dcamp },
-    // { name: 'aihub', src: aihub },
-    { name: 'abovetech', src: abovetech },
-    { name: 'mesher', src: mesher },
-    { name: 'nonce', src: nonce },
-    { name: 'desker', src: desker },
-    // { name: 'dunamu', src: dunamu },
-    // { name: 'fastventures', src: fastventures },
-    // { name: "zd", src: zd },
-    // { name: "eo", src: eo },
-    { name: 'startupStation', src: startupStation },
-    { name: 'crimson', src: crimson },
-    { name: 'koreaIndustry', src: koreaindustry },
-    { name: 'campusTown', src: campustown },
+  { name: "bass", src: bass },
+  // { name: 'codeit', src: codeit },
+  { name: "futureplay", src: futureplay },
+  { name: "mashup", src: mashup },
+  { name: "dcamp", src: dcamp },
+  // { name: 'aihub', src: aihub },
+  { name: "abovetech", src: abovetech },
+  { name: "mesher", src: mesher },
+  { name: "nonce", src: nonce },
+  { name: "desker", src: desker },
+  // { name: 'dunamu', src: dunamu },
+  // { name: 'fastventures', src: fastventures },
+  // { name: "zd", src: zd },
+  // { name: "eo", src: eo },
+  { name: "strongventures", src: strongventures },
+  { name: "zuzu", src: zuzu },
+  { name: "startupStation", src: startupStation },
+  { name: "crimson", src: crimson },
+  { name: "koreaIndustry", src: koreaindustry },
+  { name: "campusTown", src: campustown },
 ];
-
